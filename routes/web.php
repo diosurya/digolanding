c43222f93ced8a20c804127deced8726fdd8f961
Dio Putra <diosuryaputra95@gmail.com>
2026-05-23 16:47:17 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,15 @@ use App\Http\Controllers\Admin\BlogController as AdminBlogController;
 use App\Http\Controllers\BlogController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/debug-auth', function() {
+    return [
+        'db' => DB::connection()->getDatabaseName(),
+        'user_exists' => \App\Models\User::where('email', 'admin@digosoft.id')->exists(),
+        'all_users' => \App\Models\User::pluck('email'),
+        'app_url' => config('app.url'),
+    ];
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
